c58eba1f2832b91ba88b6772db8d0869140888e5
Doniyor-Programmer <74669425+Doniyor-Programmer@users.noreply.github.com>
2022-04-27 08:26:18 +0500

    fewd2

---
+++ fewd2/logicals.js
@@ -0,0 +1,7 @@
+/*
+Priority order:
+()
+===
+&& - false ni ko'rdimi ishlashni to'xtatadi. (1 && 2) => 2 bo'ladi
+|| - true ni ko'rdimi ishlashni to'xtatadi. (1 || 2) => 1 bo'ladi
+*/
---
+++ fewd2/variables.js
@@ -0,0 +1,6 @@
+// camelcase => variableName
+// snake_case => ! for js
+// UPPER_SNAKE_CASE => for constants
+// _variableName => for constants
+// Kebab-case => ! for js
+// PascalCase => most common for classes
